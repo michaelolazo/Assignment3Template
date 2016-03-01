db96779157bcdc17d8efcea201c9bb1f0e821c4f
michaelolazo <michael.olazo@robertsoncollege.net>
2016-03-01 13:49:08 -0600

    finishing the inserts and stuffs

--- A3WebApplication/A3WebApplication/Install.sql
+++ A3WebApplication/A3WebApplication/Install.sql
@@ -33,21 +33,29 @@ CREATE TABLE tbCategory
 CREATE TABLE tbProduct -- there are many products in a single category
 (			 ProductID INT IDENTITY(1,1) PRIMARY KEY,
 			 Name VARCHAR(MAX),
-			 Price INT,
+			 Price DECIMAL(10,2),
 			 PrimaryImagePath VARCHAR(MAX),
 			 CategoryID INT FOREIGN KEY REFERENCES tbCategory(CategoryID)
 			 
 )
 CREATE TABLE tbOrder -- an order happens on a date by a customer
 (			OrderID int identity(1,1) primary key,
-			--OrderDate DATE
-			
-)
+			CustomerID INT FOREIGN KEY REFERENCES tbCustomer(CustomerID),
+			CategoryID INT FOREIGN KEY REFERENCES tbCategory(CategoryID),
+			ProductID INT FOREIGN KEY REFERENCES tbProduct(ProductID)
+			/*,
+			PricePaid INT*/
+			-- come back to this later add product name and customer name?
+) 
 CREATE TABLE tbOrderDetail -- there can be many details in an order, each detail contains the product purchased, the price paid at the time for the product multiplied by the quantity
-(			OrderDetailID int identity(1,1) primary key,
+(			
+			OrderDetailID int identity(1,1) primary key,
+			PricePaid DECIMAL(10,2),
+			Quantity INT,
+			OrderDate DATE,
 			ProductID int foreign key references tbProduct(ProductID),
-			CustomerID int foreign key references tbCustomer(CustomerID)
-			
+			CustomerID int foreign key references tbCustomer(CustomerID),
+			CategoryID int foreign key references tbCategory(CategoryID)
 )
 INSERT INTO tbCustomer(FirstName,LastName,Address,City,PhoneNumber,UserName,Password,AcessLevel) VALUES
 																													 ('rj','candoy','somewhere on manitoba','WPG','204','rjayop','rjaypassword','0'),
@@ -64,26 +72,31 @@ INSERT INTO tbCategory(Name,ImagePath) VALUES
 																													('Mage','mage.png'),
 																													('Doug/Boss','dougboss.jpg') -- 4 categories
 
-INSERT INTO tbProduct(Name,Price,PrimaryImagePath,CategoryID) VALUES('Slime','15','groundedslime.jpg',1),
-																																	('Mushroom','20','groundedmushroom.png-c200',1),
-																																	('Pig','10','groundedpig.png',1),
-																																	('Zakum','200','groundedzakum.jpg',1),
-																																	('Pianus Fish Thing','1000','groundedpianus',1),
-																																	('Alf','9000','groundedalf.jpg',1),
-																																	('Pidgey','300','flyingpidgey.jpg',2),
-																																	('Noctowl','400','flyingowl.png',2),
-																																	('Super Flying OP Monster a.k.a Ice Moltres','1','flyingice.jpg',2),
-																																	('Best Mage','3','magepicture.jpg',3)
+INSERT INTO tbProduct(Name,Price,PrimaryImagePath,CategoryID)				VALUES('Slime','15.00','groundedslime.jpg',1),
+																																	('Mushroom','20.00','groundedmushroom.png-c200',1),
+																																	('Pig','10.00','groundedpig.png',1),
+																																	('Zakum','200.00','groundedzakum.jpg',1),
+																																	('Pianus Fish Thing','1000.00','groundedpianus',1),
+																																	('Alf','9000.00','groundedalf.jpg',1),
+																																	('Pidgey','300.00','flyingpidgey.jpg',2),
+																																	('Noctowl','400.00','flyingowl.png',2),
+																																	('Super Flying OP Monster a.k.a Ice Moltres','1.00','flyingice.jpg',2),
+																																	('Best Mage','3.00','magepicture.jpg',3),
+																																	('Boss.','1.00','dougboss.jpg',4)
 
 																																	  -- 6 products in category one, 3 products in category two, 1 in category three
 
-	select * from tbCustomer
-	select * from tbCategory
-	select * from tbProduct
---INSERT INTO tbOrder    -- 3 example orders from the non-admins
 
---INSERT INTO tbOrderDetail -- one item on the first order, 3 items on the second order, 2 items on the third order
+INSERT INTO tbOrder(CustomerID,ProductID,CategoryID) VALUES(2,10,3),(4,8,2),(1,11,4) -- bestmage(cost 3.00)andrew orders =3,noctowl(cost 400.00)omar orders=2,doug(cost 1.00) josh=4
+																																					     -- 3 example orders from the non-admins
 
+	SELECT * FROM tbCustomer
+	SELECT * FROM tbCategory
+	SELECT * FROM tbProduct
+	SELECT * FROM tbOrder
+		
+	INSERT INTO tbOrderDetail(CustomerID,ProductID,CategoryID,Quantity,PricePaid,OrderDate) VALUES(3,1,3,1,3,CURRENT_TIMESTAMP) -- one item on the first order, 3 items on the second order, 2 items on the third order
+	select * from tbOrderDetail
 
 ---- NOTE: All Insert procs *MUST* return the new identity number of the primary key.
 ---- Example: if you use spInsertCategory after there are 4 categories, it should return the value 5.
