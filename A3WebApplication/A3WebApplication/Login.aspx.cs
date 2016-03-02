3f91ea2f6bb2c8aee46266bd940824d31a04f64c
michaelolazo <michael.olazo@robertsoncollege.net>
2016-03-02 16:47:30 -0600

    havent been able to do proper security on the login but everyone able to login

--- A3WebApplication/A3WebApplication/Login.aspx.cs
+++ A3WebApplication/A3WebApplication/Login.aspx.cs
@@ -16,6 +16,14 @@ namespace A3WebApplication
         protected void Page_Load(object sender, EventArgs e)
         {
             // TODO: must use the Security Class to login and to check for access level (admin or not)
+            //if ((string)Session["Customer"]!=null)
+            //{
+            //    lblMessage.Text = "Welcome to our village session fam";
+            //}
+            //else
+            //{
+            //    lblMessage.Text = "Try again fam session fked";
+            //}
         }
 
         protected void btnLogin_Click(object sender, EventArgs e)
@@ -24,11 +32,33 @@ namespace A3WebApplication
             DAL myd = new DAL(ConfigurationManager.ConnectionStrings["dbA3ConnStr"].ConnectionString);
             myd.AddParam("@UserName", tbUsername.Text);
             myd.AddParam("@Password", tbPassword.Text);
-            DataSet ds = myd.ExecuteProcedure("spLogin");
-           // lblMessage.Text = "Welcome to our Village";
-            if ()
-            {
 
+            DataSet ds = myd.ExecuteProcedure("spLogin");
+            //Session["Customer"] = (ds.Tables[0].Rows[0]["Customer"]);
+          //  Session[""] = (ds.Tables[0].Rows[0]["ColumnName"]);
+            // lblMessage.Text = "Welcome to our Village";
+            switch (tbUsername.Text)
+            {
+                case "michaeladmin":
+                    lblMessage.Visible = true;
+                    lblMessage.Text = "welcome to our village admin michael";
+                    break;
+                case "rjayop":
+                    lblMessage.Visible = true;
+                    lblMessage.Text = "welcome customer rjay";
+                    break;
+                case "omar":
+                    lblMessage.Visible = true;
+                    lblMessage.Text = "welcome customer omar";
+                    break;
+                case "josh":
+                    lblMessage.Visible = true;
+                    lblMessage.Text = "welcome customer josh";
+                    break;
+                default:
+                    lblMessage.Visible = true;
+                    lblMessage.Text = "wrong info try again fam";
+                    break;
             }
             //if (ds.Tables[0].Rows.Count != 1)
             //{
